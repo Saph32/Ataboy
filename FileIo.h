53210b9e4e060165032d330da4afe0ed1b363149
Saph32 <saph32@users.noreply.github.com>
2017-07-15 18:48:53 -0400

    Fix Linux compilation

--- FileIo.h
+++ FileIo.h
@@ -1,8 +1,9 @@
 #pragma once
 
 #include <vector>
+#include <string>
 
 std::vector<char> LoadFile(const char* file_name);
 std::string GetSaveFileName(const char* gamepak_file_name);
-std::vector<char> LoadSaveFile(const char* file_name, const size_t expected_size);
-bool SaveSaveFile(const char* file_name, const char* data, const size_t size);
+std::vector<char> LoadSaveFile(const char* file_name, const std::size_t expected_size);
+bool SaveSaveFile(const char* file_name, const char* data, const std::size_t size);
